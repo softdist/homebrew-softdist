f581762519c872c941caafdb361af69b3f9aef99
Lyns <lotsofthoughts@lynsei.com>
2025-03-11 13:56:30 -0400

    v0.2.1; Update Class Name
    Signed-off-by: Lyns <lotsofthoughts@lynsei.com>

--- Formula/softdist-release.rb
+++ Formula/softdist-release.rb
@@ -4,22 +4,14 @@ class SoftdistRelease < Formula
   version "main"
   license "MIT"
 
-  # Variable to store downloaded binary name
-  def initialize(*)
-    super
-    @run_binary = nil
-  end
-
   # Main binary URL
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/softdist/docker.client/releases/download/main/sd-run-darwin-amd64"
       sha256 "cc102989665bc7aec81e3319a65375782b7e46262968ffb92b7a47bc10761b63"
-      def initialize(*); super; @run_binary = "sd-run-darwin-amd64"; end
     elsif Hardware::CPU.arm?
       url "https://github.com/softdist/docker.client/releases/download/main/sd-run-darwin-arm64"
       sha256 "d6ebdef1d01d6b75ea28c7a8a711f3bbb26384dc16812f3fe3aafa2fc83e0c08"
-      def initialize(*); super; @run_binary = "sd-run-darwin-arm64"; end
     end
   end
 
@@ -27,25 +19,21 @@ class SoftdistRelease < Formula
     if Hardware::CPU.intel?
       url "https://github.com/softdist/docker.client/releases/download/main/sd-run-linux-amd64"
       sha256 "b98c9f21a55af87335d1ab59679dcaab0d8f3b09769a35c3a7f8c3a7cbce5b1f"
-      def initialize(*); super; @run_binary = "sd-run-linux-amd64"; end
     elsif Hardware::CPU.arm?
       url "https://github.com/softdist/docker.client/releases/download/main/sd-run-linux-arm64"
       sha256 "d599c462a4875799d4f28b91380692b5ad66130ff8cd7af6d403d3d0e35618ad"
-      def initialize(*); super; @run_binary = "sd-run-linux-arm64"; end
     end
   end
 
-  # Define resources only (no install here)
+  # sd-install resource
   resource "sd-install" do
     on_macos do
       if Hardware::CPU.intel?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-install-darwin-amd64"
         sha256 "62c40b960b9fed52bf391d0dd91496ac9a7ee80311eb01370712b2574b03bd89"
-        def initialize(*); super; @run_binary = "sd-install-darwin-amd64"; end
       elsif Hardware::CPU.arm?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-install-darwin-arm64"
         sha256 "736d26032ee788eead96be3ebb4eee61694fbb3e6476abca0e896b4d058f9d9e"
-        def initialize(*); super; @run_binary = "sd-run-darwin-arm64"; end
       end
     end
 
@@ -53,25 +41,22 @@ class SoftdistRelease < Formula
       if Hardware::CPU.intel?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-install-linux-amd64"
         sha256 "70c767689c5623316ccdc65792c42a20bc10320860b7450e6d9ed55ac06e1905"
-        def initialize(*); super; @run_binary = "sd-install-linux-amd64"; end
       elsif Hardware::CPU.arm?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-install-linux-arm64"
         sha256 "c107fdd5b629832a99aa845ff692dd6bda033c60cc86e6bd3c5c2f6d57fecf2c"
-        def initialize(*); super; @run_binary = "sd-install-linux-arm64"; end
       end
     end
   end
 
+  # sd-extricate resource
   resource "sd-extricate" do
     on_macos do
       if Hardware::CPU.intel?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-darwin-amd64"
         sha256 "8a0427ac0d29cd4bbe816c56b7c4c76a13a641b33aa4406e7f63c346a014ce87"
-        def initialize(*); super; @run_binary = "sd-extricate-darwin-amd64"; end
       elsif Hardware::CPU.arm?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-darwin-arm64"
         sha256 "362e123bf6035ce5279ba4e1a6b5fc5ec01a1af06d3c9cb7b2970c3bb2899cfe"
-        def initialize(*); super; @run_binary = "sd-extricate-darwin-arm64"; end
       end
     end
 
@@ -79,30 +64,38 @@ class SoftdistRelease < Formula
       if Hardware::CPU.intel?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-linux-amd64"
         sha256 "cb7ec36dafe0a2290bc262f73e0ca2e25036338c3b2922b000875aa75a276fdd"
-        def initialize(*); super; @run_binary = "sd-extricate-linux-amd64"; end
       elsif Hardware::CPU.arm?
         url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-linux-arm64"
         sha256 "cf7c99e3a80ad167b7a5f30e375a7d3d74e19142058fa28669f7bd1f553aa78c"
-        def initialize(*); super; @run_binary = "sd-extricate-linux-arm64"; end
       end
     end
   end
 
-  # Install the binaries
+  # Correct install method with explicit handling
   def install
-    # Install main binary properly
-    bin.install @run_binary => "sd-run"
+    # Detect the right filename and rename
+    arch = if OS.mac? && Hardware::CPU.intel?
+      "darwin-amd64"
+    elsif OS.mac? && Hardware::CPU.arm?
+      "darwin-arm64"
+    elsif OS.linux? && Hardware::CPU.intel?
+      "linux-amd64"
+    elsif OS.linux? && Hardware::CPU.arm?
+      "linux-arm64"
+    end
 
-    # Install other binaries
+    bin.install "sd-run-#{arch}" => "sd-run"
+
     resource("sd-install").stage do
-      bin.install Dir["*"].first => "sd-install"
+      bin.install "sd-install-#{arch}" => "sd-install"
     end
 
     resource("sd-extricate").stage do
-      bin.install Dir["*"].first => "sd-extricate"
+      bin.install "sd-extricate-#{arch}" => "sd-extricate"
     end
   end
 
+  # Tests
   test do
     system "#{bin}/sd-run", "--version"
     system "#{bin}/sd-install", "--version"
