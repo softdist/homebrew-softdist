2c9e4086f73deefbe10063ae3e40f9e0e6ec06f3
Lyns <lotsofthoughts@lynsei.com>
2025-03-11 12:12:01 -0400

    Updated install
    Signed-off-by: Lyns <lotsofthoughts@lynsei.com>

--- Formula/softdist-release.rb
+++ Formula/softdist-release.rb
@@ -1,36 +1,78 @@
-class SoftdistRelease < Formula
-  desc "Docker client emulator"
+class Sd < Formula
+  desc "SD Tools: sd-run, sd-install, sd-extricate"
   homepage "https://github.com/softdist/docker.client"
-  version "0.1.8"
+  version "main"
   license "MIT"
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/softdist/docker.client/releases/download/v0.1.8/docker.client-darwin-arm64"
-    sha256 "<SHA256_MAC_ARM64>"
-  elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/softdist/docker.client/releases/download/v0.1.8/docker.client-darwin-amd64"
-    sha256 "<SHA256_MAC_AMD64>"
-  elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/softdist/docker.client/releases/download/v0.1.8/docker.client-linux-arm64"
-    sha256 "<SHA256_LINUX_ARM64>"
-  elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/softdist/docker.client/releases/download/v0.1.8/docker.client-linux-amd64"
-    sha256 "<SHA256_LINUX_AMD64>"
-  else
-    odie "Unsupported architecture"
-  end
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/softdist/docker.client/releases/download/main/sd-run-darwin-amd64"
+      sha256 "SHA256_FOR_SD_RUN_DARWIN_AMD64"
 
-  def install
-    bin.install "docker.client"
-  end
+      resource "sd-install" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-install-darwin-amd64"
+        sha256 "SHA256_FOR_SD_INSTALL_DARWIN_AMD64"
+      end
 
-  def post_install
-    if OS.mac?
-      system "codesign", "--force", "--deep", "--sign", "-", "#{bin}/docker.client"
+      resource "sd-extricate" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-darwin-amd64"
+        sha256 "SHA256_FOR_SD_EXTRICATE_DARWIN_AMD64"
+      end
+    elsif Hardware::CPU.arm?
+      url "https://github.com/softdist/docker.client/releases/download/main/sd-run-darwin-arm64"
+      sha256 "SHA256_FOR_SD_RUN_DARWIN_ARM64"
+
+      resource "sd-install" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-install-darwin-arm64"
+        sha256 "SHA256_FOR_SD_INSTALL_DARWIN_ARM64"
+      end
+
+      resource "sd-extricate" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-darwin-arm64"
+        sha256 "SHA256_FOR_SD_EXTRICATE_DARWIN_ARM64"
+      end
     end
   end
 
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/softdist/docker.client/releases/download/main/sd-run-linux-amd64"
+      sha256 "SHA256_FOR_SD_RUN_LINUX_AMD64"
+
+      resource "sd-install" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-install-linux-amd64"
+        sha256 "SHA256_FOR_SD_INSTALL_LINUX_AMD64"
+      end
+
+      resource "sd-extricate" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-linux-amd64"
+        sha256 "SHA256_FOR_SD_EXTRICATE_LINUX_AMD64"
+      end
+    elsif Hardware::CPU.arm?
+      url "https://github.com/softdist/docker.client/releases/download/main/sd-run-linux-arm64"
+      sha256 "SHA256_FOR_SD_RUN_LINUX_ARM64"
+
+      resource "sd-install" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-install-linux-arm64"
+        sha256 "SHA256_FOR_SD_INSTALL_LINUX_ARM64"
+      end
+
+      resource "sd-extricate" do
+        url "https://github.com/softdist/docker.client/releases/download/main/sd-extricate-linux-arm64"
+        sha256 "SHA256_FOR_SD_EXTRICATE_LINUX_ARM64"
+      end
+    end
+  end
+
+  def install
+    bin.install "sd-run"
+    bin.install resource("sd-install")
+    bin.install resource("sd-extricate")
+  end
+
   test do
-    system "#{bin}/docker.client", "--version"
+    system "#{bin}/sd-run", "--version"
+    system "#{bin}/sd-install", "--version"
+    system "#{bin}/sd-extricate", "--version"
   end
 end
